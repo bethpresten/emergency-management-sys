4e3975511dd045c66344354cd272c074052f887a
Beth Presten <bethpresten@gmail.com>
2020-11-20 16:21:34 -0500

    retriving all employees working

--- index.js
+++ index.js
@@ -13,54 +13,63 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    connection.query("SELECT * FROM employees", (err, res) => {
-        if (err) throw err;
-        console.table(res)
-    });
-
-    const promptUser = () => {
-        return inquirer.prompt([
-            {
-                type: "list",
-                name: "name",
-                message: "What would you like to do?",
-                choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add employee", "Remove employee", "Update employee role", "Update employee manager"]
-            }
-            //         {
-            //             type: "number",
-            //             name: "id",
-            //             message: "What is your employee ID number?",
-            //         },
-            //         {
-            //             type: "input",
-            //             name: "email",
-            //             message: "What is your email address?",
-            //         },
-            //         {
-            //             type: "input",
-            //             name: "officeNumber",
-            //             message: "What is your office number?",
-            //         },
-            //         {
-            //             type: "validate",
-            //             name: "addEmployee",
-            //             message: "Would you like to add another employee??",
-            //         },
-            //         // pushing all the information to the constructor
-        ])
-    };
-    // .then() {
-    //         const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
-    //         employees.push(manager);
-    //         // asking to add additional employees or writing to the HTML if finished
-    //         console.log(managerAnswers);
-    //         if (managerAnswers.addEmployee === 'y') {
-    //             return employeeQuestions();
-    //         } else {
-    //             return writeHTML();
-    //         }
-    //     });
-    // };
-    promptUser();
+    // connection.query("SELECT * FROM employees", (err, res) => {
+    //     if (err) throw err;
+    //     console.table(res);
+    // connection.end();
 });
 
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add employee", "Remove employee", "Update employee role", "Update employee manager"]
+        }
+        //         {
+        //             type: "number",
+        //             name: "id",
+        //             message: "What is your employee ID number?",
+        //         },
+        //         {
+        //             type: "input",
+        //             name: "email",
+        //             message: "What is your email address?",
+        //         },
+        //         {
+        //             type: "input",
+        //             name: "officeNumber",
+        //             message: "What is your office number?",
+        //         },
+        //         {
+        //             type: "validate",
+        //             name: "addEmployee",
+        //             message: "Would you like to add another employee??",
+        //         },
+        //         // pushing all the information to the constructor
+    ]).then(response => {
+        if (response.action === "View all employees") {
+            allEmployees();
+        } else (response.action === "View all employees by department"){
+            employeesByDepartment();
+        }
+
+
+
+    })
+
+};
+
+allEmployees = () => {
+    connection.query("SELECT * FROM employee", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    });
+};
+employeesByDepartment = () => {
+
+}
+
+promptUser();
